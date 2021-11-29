fcb90d84767a12dec836c9da2ef58853b6beae9e
sumant9822 <sumantsantosh@benchmarkit.solutions>
2021-11-29 14:36:56 +0530

    crud

--- CRUD/script.js
+++ CRUD/script.js
@@ -1,60 +1,142 @@
 
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
-	var actions = $("table td:last-child").html();
-	// Append table with add row form on add new button click
-    $(".add-new").click(function(){
-		$(this).attr("disabled", "disabled");
-		var index = $("table tbody tr:last-child").index();
-        var row = '<tr>' +
-            '<td><input type="text" class="form-control" name="gname" id="name"></td>' +
-            '<td><input type="text" class="form-control" name="series" id="series"></td>' +
-            '<td><input type="date" id="date" class="form-control" name="released" id="released"></td>' +
-            '<td><input type="text" class="form-control" name="developer" id="developer"></td>' +
-            '<td><input type="text" class="form-control" name="publisher" id="publisher"></td>' +
-            '<td><input type="text" class="form-control" name="genres" id="genres"></td>' +
-			'<td>' + actions + '</td>' +
-        '</tr>';
-    	$("table").append(row);		
-		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-	// Add row on add button click
-	$(document).on("click", ".add", function(){
-		var empty = false;
-		var input = $(this).parents("tr").find('input[type="text"]');
-        input.each(function(){
-			if(!$(this).val()){
-				$(this).addClass("error");
-				empty = true;
-			} else{
-                $(this).removeClass("error");
-            }
-		});
-		$(this).parents("tr").find(".error").first().focus();
-		if(!empty){
-			input.each(function(){
-				$(this).parent("td").html($(this).val());
-			});			
-			$(this).parents("tr").find(".add, .edit").toggle();
-			$(".add-new").removeAttr("disabled");
-		}	
-        $('#date').get(0).type = 'text';	
-    });
-	// Edit row on edit button click
-	$(document).on("click", ".edit", function(){		
-        $(this).parents("tr").find("td:not(:last-child)").each(function(){
-            
-			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-            // $('#date').get(0).type = 'date';
-            // $(this).html('<input type="date" id="date" class="form-control" value="' + $(this).text() + '">');
-		});		
-		$(this).parents("tr").find(".add, .edit").toggle();
-		$(".add-new").attr("disabled", "disabled");
-    });
-	// Delete row on delete button click
-	$(document).on("click", ".delete", function(){
-        $(this).parents("tr").remove();
-		$(".add-new").removeAttr("disabled");
-    });
+    // Add note to local storage
+	
+	
+let addBtn = document.getElementById("adds");
+addBtn.addEventListener("click", function(e) {
+console.log("clicked");
+  let gname = document.getElementById("gname");
+  let series = document.getElementById("gseries");
+  let space = document.getElementById("gspace");
+  let developer = document.getElementById("gdeveloper");
+  let publisher = document.getElementById("gpublisher");
+  let genres = document.getElementById("ggenres");
+  
+    if (gname.value == "" || series.value == "" || space.value == "" || developer.value == "" || publisher.value == "" || genres.value == "" ) {
+        return alert("Please add all the fields ")
+    }
+
+  let gdetail = localStorage.getItem("gamedetails");
+  if (gdetail == null) {
+    gameObj = [];
+  } else {
+    gameObj = JSON.parse(gdetail);
+  }
+  let myObj = {
+    name: gname.value,
+    series: series.value,
+	space: space.value,
+	developer: developer.value,
+	publisher: publisher.value,
+	genres: genres.value
+  }
+  gameObj.push(myObj);
+  localStorage.setItem("gamedetails", JSON.stringify(gameObj));
+  gname.value = "";
+  series.value = "";
+  space.value ="";
+  developer.value="";
+  publisher.value="";
+  genres.value="";
+   console.log(gameObj);
+  showGames();
 });
+
+// Function to show elements from localStorage
+function showGames() {
+	let gdetail = localStorage.getItem("gamedetails");
+	if (gdetail == null) {
+	  gameObj = [];
+	} else {
+	  gameObj = JSON.parse(gdetail);
+	}
+
+	let html = "";
+	gameObj.forEach(function(g, gid) {
+	  html += `
+	  <tbody class="gamedetails">
+	  <tr >
+	                     <td class="gid">${gid + 1}</td>
+                        <td class ="gname">${g.name}</td>
+                        <td class="gseries">${g.series}</td>
+                        <td class="gspace">${g.space}</td>
+                        <td class="gdeveloper">${g.developer}</td>
+                        <td class="gpublisher">${g.publisher}</td>
+                        <td class="ggenres">${g.genres}</td>
+	  <td>
+		  <a class="edit" id=${gid} onclick="deleteGame(this.id)" title="Delete" ><i class="material-icons"> &#xE872;</i></a>
+		  <a class="delete" id=${gid} onclick="editGame(this.id)" title="Edit" ><i class="material-icons"> &#xE254;</i></a>
+	  </td>
+  </tr> </tbody>
+			  `;
+	});
+	let gameElm = document.getElementById("gamedetails");
+	
+	if (gameObj.length != 0) {
+	  gameElm.innerHTML = html;
+	} else {
+	  gameElm.innerHTML = `No games entered.`;
+	}
+  }  
+
+  showGames();
+ 
+  
+  //edit
+function editGame(gid) {
+	 let gdetail = localStorage.getItem("gamedetails");
+	let gname1 = document.getElementById("gname");
+	let series1 = document.getElementById("gseries");
+	let space1 = document.getElementById("gspace");
+	let developer1 = document.getElementById("gdeveloper");
+	let publisher1 = document.getElementById("gpublisher");
+	let genres1 = document.getElementById("ggenres");
+
+	// if (gname.value == "" || series.value == "" || space.value == "" || developer.value == "" || publisher.value == "" || genres.value == "" ) {
+    //     return alert("Please add all the fields ")
+    // }
+
+    if (gdetail == null) {
+    gameObj = [];
+    } else {
+      gameObj = JSON.parse(gdetail);
+    }
+    console.log(gameObj);
+
+    gameObj.findIndex((g, gid) => {
+		gname1.value = g.name;
+		series1.value = g.series;
+		space1.value =g.space;
+		developer1.value=g.developer;
+		publisher1.value=g.publisher;
+		genres1.value=g.genres;
+    })
+    gameObj.splice(gid, 1);
+        localStorage.setItem("gamedetails", JSON.stringify(gameObj));
+        
+}
+
+showGames();
+// Function to delete a note
+function deleteGame(gid) {
+	//   console.log("I am deleting", gid);
+		let confirmDel = confirm("Do you want to delete");
+		if (confirmDel == true) {
+			let gdetail = localStorage.getItem("gamedetails");
+			if (gdetail == null) {
+				gameObj = [];
+			} else {
+				gameObj = JSON.parse(gdetail);
+			}
+	
+			gameObj.splice(gid, 1);
+			localStorage.setItem("gamedetails", JSON.stringify(gameObj));
+			showGames();
+		}
+	  
+	}
+
+
+
+
+
